8abd6348dd750976ce50bfbf15a5a5f34cf7b000
“anstarky” <anstarky@gmail.com>
2019-10-26 16:05:04 +0300

    Updates

---
+++ precache-manifest.16c57d1f5fb5768f24156e782ecf2fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fdd35ccb81973cb0595b4998b35bfd",
+    "url": "/anstarky/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "d620f996cdf188653bcf",
+    "url": "/anstarky/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c3aa32d9fdd4235bb739",
+    "url": "/anstarky/goit-react-hw-01-components/static/css/main.bde7cf86.chunk.css"
+  },
+  {
+    "revision": "d620f996cdf188653bcf",
+    "url": "/anstarky/goit-react-hw-01-components/static/js/2.91291085.chunk.js"
+  },
+  {
+    "revision": "c3aa32d9fdd4235bb739",
+    "url": "/anstarky/goit-react-hw-01-components/static/js/main.c8686dd0.chunk.js"
+  },
+  {
+    "revision": "bc307e9170104603895e",
+    "url": "/anstarky/goit-react-hw-01-components/static/js/runtime-main.6a80d7b1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anstarky/goit-react-hw-01-components.git/precache-manifest.9b2db97bfb5b4d8dda973f814d418399.js"
+  "/anstarky/goit-react-hw-01-components/precache-manifest.16c57d1f5fb5768f24156e782ecf2fa5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anstarky/goit-react-hw-01-components.git/index.html"), {
-
-  blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anstarky/goit-react-hw-01-components/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
